fb7de1b8a5fcabdeec8c1beb60b11cd3b43d7614
(Donald) Max Harkins <harkindo@oregonstate.edu>
2019-05-03 12:03:43 -0700

    added unit test (wip) and neccesary part to makefile

--- dominion/unittest1.c
+++ dominion/unittest1.c
@@ -12,31 +12,49 @@
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
+#include <stdbool.h>
 #include "rngs.h"
+
+#define TESTCARD "council room"
 
 // set NOISY_TEST to 0 to remove printfs from output
 #define NOISY_TEST 1
 
-int main() {
-    int newCards = 0;
-    int discarded = 1;
-    int xtraCoins = 0;
-    int shuffledCards = 0;
+bool assertTrue(bool b, char * errMsg){
+    if (b) {
+        return true;
+    }
+    else {
+        printf("Error: did not assert '%s'\n", errMsg);
+        return false;
+    }
+}
 
-    int i, j, m;
-    int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
-    int remove1, remove2;
+int main() {
+    bool unitTestPassed = true;
+
+    //int i, j, m;
+    //int remove1, remove2;
 
 
    	struct gameState G, testG;
     int seed = 1000;
     int numPlayers = 2;
-    int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
-    			sea_hag, tribute, smithy, council_room};
+    int k[10] = {council_room, embargo, village, minion, mine, cutpurse,
+    			sea_hag, tribute, smithy, adventurer};
 
 	// initialize a game state and player cards
 	initializeGame(numPlayers, k, seed, &G);
 
+    printf("----------------- Testing Card: %s ----------------\n", TESTCARD);
 
+    memcpy(&testG, &G, sizeof(struct gameState));
 
+    councilRoomEffect(council_room, &testG, 0);
+    int currentPlayer = whoseTurn(&testG);
+    if (!assertTrue(testG.discardCount[currentPlayer] == 1, "1 card discarded") )
+        unitTestPassed = false;
+
+    if (unitTestPassed)
+        printf("Unit Test Passed!\n");
 }
