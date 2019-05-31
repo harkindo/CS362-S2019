f102cb5038c60efd4b0577358ce14771a01fab27
(Donald) Max Harkins <harkindo@oregonstate.edu>
2019-05-31 14:18:10 -0700

    Fixed bug where Baron card not discarded from hand after play

--- projects/harkindo/landrevjDominion/dominion.c
+++ projects/harkindo/landrevjDominion/dominion.c
@@ -1087,6 +1087,8 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       }
     }
 
+    discardCard(handPos, currentPlayer, state, 0); // Discard Baron card
+
     return 0;
 
   case great_hall:
